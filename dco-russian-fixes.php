0abeff9a6b46b3b51bcbe6d5138ed2fc0c62b811
denisco <denisco@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-13 18:54:46 +0000

    Optimized Updated readme
    git-svn-id: https://plugins.svn.wordpress.org/dco-russian-fixes/tags/1.0.3@1112166 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ dco-russian-fixes.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+  Plugin Name: DCO russian fixes
+  Plugin URI: https://github.com/Denis-co/dco-russian-fixes
+  Description: Add Wordpress russian language fixes
+  Version: 1.0.3
+  Author: Denis co.
+  Author URI: http://denisco.pro
+  License: GPLv2 or later
+  Text Domain: dco-rf
+ */
+
+define( 'DCO_RF__PLUGIN_URL', plugin_dir_url( __FILE__ ) );
+define( 'DCO_RF__PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
+
+require_once( DCO_RF__PLUGIN_DIR . 'class.dco-russian-fixes-base.php' );
+require_once( DCO_RF__PLUGIN_DIR . 'class.dco-russian-fixes.php' );
+if ( is_admin() ) {
+	require_once( DCO_RF__PLUGIN_DIR . 'class.dco-russian-fixes-admin.php' );
+}
